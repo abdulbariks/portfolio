48189ff8b020a202665c3dc25eb47c2a1bab1a2f
Abdul Barik <abdulbarik1997m@gmail.com>
2024-01-10 01:43:49 +0600

    NextJs Portfolio Project

---
+++ app/components/AboutSection.jsx
@@ -0,0 +1,116 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Next</li>
+        <li>NodeJS</li>
+        <li>Express</li>
+        <li>MongoDB</li>      
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Bachelor of Science (BSc):<br></br>
+              Institution : UCASM Under of National University<br></br>
+              Subject : Computer Science Engineering(CSE)<br></br>
+              CGPA : 2.68</li>
+        <li>Higher Secondary Certificate (HSC):<br></br>
+              Institution : Bhawal Badre Alam Govt College , Gazipur<br></br>
+              Group : Science<br></br>
+              GPA : 3.42<br></br>
+              Board : Dhaka</li>
+        <li>Secondary School Certificate (SSC):<br></br>
+              Institution : Tista K R Khadem High School, Tista, Lalmonirhat<br></br>
+              Group : Science<br></br>
+              GPA : 4.56<br></br>
+              Board : Dinajpur
+        </li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Full Stack Development-Bohubrihi</li>
+        <li>Mobile Application Development-BITM</li>
+        <li>Search Engine Optimization(SEO)-eShikhon</li>
+        <li>Web Development-CodersTrust Bangladesh</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a full stack web developer with a passion for creating
+            interactive and responsive web applications. I have experience
+            working with HTML, CSS, JavaScript, React, Next, Node.js, Express, MongoDB, 
+            and Git. I am a quick learner and I am always
+            looking to expand my knowledge and skill set. I am a team player and
+            I am excited to work with others to create amazing applications.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
