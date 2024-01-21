4220d049554d258a709aa3469c5fdd0aaa432d08
Abdul Barik <abdulbarik1997m@gmail.com>
2024-01-22 01:24:45 +0600

    metadata added

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Abdu Barik',
+  title: 'Abdul Barik',
   description: `It's My Potfolio Website`,
 }
 
