33945d7c29d9af3fa6055fcae1839f77db40ac53
Abdul Barik <abdulbarik1997m@gmail.com>
2025-06-29 03:49:57 +0600

    design changed

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -93,6 +93,9 @@ const AboutSection = () => {
 
   return (
     <section className="text-white mt-16" id="about">
+      <h2 className="text-4xl text-center font-bold text-white mb-4">
+        About Me
+      </h2>
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image
           src="/images/about-image.png"
@@ -102,7 +105,6 @@ const AboutSection = () => {
           className="rounded-md"
         />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
             I am a full stack web developer with a passion for creating
             interactive and responsive web applications. I have experience
